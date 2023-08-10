ca2d151c26e7d07d615900578fe1197754966ef0
dileep <pateldileep290492@gmail.com>
2023-08-10 17:43:18 +0530

    navbar changes

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,6 +4,10 @@ import logo from '../../image/logo.png'
 
 
 const Navbar = () => {
+ 
+const handlemobile = () =>{
+    console.log("clicked")
+}
     return (
         <>
             <nav>
@@ -13,19 +17,44 @@ const Navbar = () => {
                     </div>
                     <div className="search-bar">
                         <input type="text" placeholder='Search' />
-                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <i className="fa-solid fa-magnifying-glass"></i>
                     </div>
                 </div>
                 <div className="menu-bottom">
-                    <ul>
-                        <li>आचार्य-जी</li>
-                        <li>संघ</li>
-                        <li>महोत्सव</li>
-                        <li>चातुर्मास</li>
+
+                    <ul className='menu'>
+                        <li>
+                            <a href="/">आचार्य-जी</a>
+                            <ul>
+                                <li>
+                                    <a href="/">जीवन-परिचय</a>
+                                </li>
+                                <li>
+                                    <a href="/">त्याग</a>
+                                </li>
+                                <li>
+                                    <a href="/">आरती</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/">संघ</a>
+                        </li>
+                        <li>
+                            <a href="/">महोत्सव</a>
+                        </li>
+                        <li>
+                            <a href="/">चातुर्मास</a>
+                        </li>
                     </ul>
+                    <div className="hamburger" onClick={handlemobile}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
                 </div>
             </nav>
-            
+
         </>
     )
 }
